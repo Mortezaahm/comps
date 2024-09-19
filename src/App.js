6c66f8a8b05f2e7cd55bee286221469f7bdc3dbe
Morteza <morteza.ahmadianmanzary@gmail.com>
2024-09-19 20:26:11 +0200

    Button component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { GoBellFill, GoCloud, GoDatabase } from "react-icons/go";
+import Button from "./Button";
 function App() {
   return (
     <div>
-      <h1>React App</h1>
+      <div>
+        <Button secondary>
+          <GoBellFill /> Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button success outline>
+          <GoCloud />
+          Buy Now
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoDatabase />
+          See Deals
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads
+        </Button>
+      </div>
+      <div>
+        <Button primary>Cancel</Button>
+      </div>
     </div>
   );
 }
